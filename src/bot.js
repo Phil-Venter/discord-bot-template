f8212636e32d63dd9f378a26b8d5c35f4fba0d2f
Philip <philip@datasmart.co.za>
2021-11-26 22:48:53 +0200

    entry file update

--- src/bot.js
+++ src/bot.js
@@ -1,27 +1,21 @@
 const { Client, Intents } = require('discord.js');
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [ Intents.FLAGS.GUILDS ] });
+
 client.config = require('../config.json');
 
 const files = require('./files.js');
 
-const handlerFiles = files('./src/handlers');
-const databaseFiles = files('./src/database');
-const eventFiles = files('./src/events');
-const buttonFiles = files('./src/buttons');
-const commandFiles = files('./src/commands');
-const selectFiles = files('./src/select');
-
 (async () => {
-	for (file of handlerFiles) {
+	for (file of files('./src/handlers')) {
 		require(file)(client);
 	}
 
-	client.handleDatabase(databaseFiles);
-	client.handleEvents(eventFiles);
-	client.handleButtons(buttonFiles);
-	client.handleCommands(commandFiles);
-	client.handleSelects(selectFiles);
+	client.handleDatabase(files('./src/database'));
+	client.handleEvents(files('./src/events'));
+	client.handleButtons(files('./src/buttons'));
+	client.handleCommands(files('./src/commands'));
+	client.handleSelects(files('./src/select'));
 
-	client.login(config.token);
+	client.login(client.config.token);
 })();
